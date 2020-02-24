bc99017fe9d6a29bbcfd965dbfbb1fe184df673f
holzkeil <hdz@gmx.ch>
2020-02-24 20:50:51 +0100

    code cleanup

--- temp-sensor-client.ino
+++ temp-sensor-client.ino
@@ -11,30 +11,34 @@
 // sub interval measuring
 // display show last avarage, interval counter, and current sub interval
 
-#define TEMPSENSORPIN 4
-#define TEMPRESOLUTION 12
+#define PIN_TEMPSENSOR 4
+#define TEMPRESOLUTION 12  // resolution 12 (highest) to 9 (lowest)
 #define FLOAT_LENGTH 1
-#define BUTTONPIN 7
-#define LEDPIN 6
+#define PIN_BUTTON 7
+#define PIN_LED 6
 
-#define RADIO_ID 1             // Our radio's id.
-#define DESTINATION_RADIO_ID 0 // Id of the radio we will transmit to.
+#define SOURCE_RADIO_ID 1
+#define DESTINATION_RADIO_ID 0
 #define PIN_RADIO_CE 2
 #define PIN_RADIO_CSN 3
 
 #define INTERVAL_VALUE_COUNT 16
 #define INTERVAL_EEPROM_ADDRESS 0
 
-OneWire oneWire(TEMPSENSORPIN); 
+#define PIN_OLED_CHIP_SELECT_DS 10
+#define PIN_OLED_DATA_COMMAND 9
+#define PIN_OLED_RESET 8
+
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_HEADER 16
+#define DISPLAY_WIDTH 128
+
+OneWire oneWire(PIN_TEMPSENSOR); 
 DallasTemperature sensors(&oneWire);
+U8G2_SSD1306_128X64_VCOMH0_1_4W_HW_SPI u8g2(U8G2_R0, PIN_OLED_CHIP_SELECT_DS, PIN_OLED_DATA_COMMAND, PIN_OLED_RESET);
 
-#define OLED_CS 10 // Chip Select, DS
-#define OLED_DC 9 // Data/Command
-#define OLED_RESET 8
-
-U8G2_SSD1306_128X64_VCOMH0_1_4W_HW_SPI u8g2(U8G2_R0, OLED_CS, OLED_DC, OLED_RESET);
-
-struct RadioPacket // Any packet up to 32 bytes can be sent.
+// max 32 bytes
+struct RadioPacket
 {
     uint8_t FromRadioId;
     uint32_t OnTimeMillis;
@@ -47,7 +51,7 @@ class Task {
     unsigned long previous_millis;
     unsigned long current_millis;
     unsigned long interval_values[INTERVAL_VALUE_COUNT] = {1000, 2000, 5000, 10000, 15000, 30000, 60000, 120000, 300000, 600000, 675000, 1012500, 1350000, 2025000, 4725000, 9450000};
-    uint8_t interval_pointer = 0;
+    int8_t interval_pointer = 0;
     void (*fun)();
     String func_name;
 
@@ -58,11 +62,7 @@ class Task {
       fun = func;
       if (String("temp") == name){
         EEPROM.get(INTERVAL_EEPROM_ADDRESS, interval_pointer);
-        interval_pointer %= INTERVAL_VALUE_COUNT;
-        interval_pointer--;
-        if (interval_pointer == -1){
-          interval_pointer = 15;
-        }      
+        interval_pointer--;     
         change_interval();
       }
       else{
@@ -73,7 +73,7 @@ class Task {
       return;
     }
 
-    void change_interval(void){
+    void change_interval(){
       interval_pointer = ++interval_pointer % INTERVAL_VALUE_COUNT;
       interval = interval_values[interval_pointer];
       Serial.println("Interval: " + String(interval) + "IntervalPointer: " + String(interval_pointer));
@@ -90,48 +90,42 @@ class Task {
     }
 };
 
-
 class TemperatureArray {
    public:
      uint8_t current = 0;
-     float minimum = 10000;
-     float maximum= -10000;
-     const static uint8_t count = 128;
-     float values[count] = {};
+     float minimum;
+     float maximum;
+     float values[DISPLAY_WIDTH] = {};
      bool init_done = false;
      bool isDrawn = false;
-     uint8_t display_height;
-     uint8_t header_height;
      float delta;
      float delta_per_pixel;
     
-    TemperatureArray(uint8_t dheight, uint8_t hheight){
-      display_height = dheight;
-      header_height = hheight;
+    TemperatureArray(){
       return;
     }
 
   void add(float temp){
     if (!init_done){
-      for (int i=0; i < count; i++){
+      for (int i=0; i < DISPLAY_WIDTH; i++){
         values[i] = temp;
       }
       init_done = true;
     }
     values[current] = temp;
-    current = ++current % count;
+    current = ++current % DISPLAY_WIDTH;
     minimum = values[0];
     maximum = values[0];
-    for (uint8_t i=1;i<128;i++){
+    for (uint8_t i=1;i<DISPLAY_WIDTH;i++){
       if (values[i] < minimum) minimum = values[i];
       if (values[i] > maximum) maximum = values[i];
     }
     delta = maximum - minimum;
     if (delta != 0.0){ 
-      delta_per_pixel = delta / (display_height - header_height);
+      delta_per_pixel = delta / (DISPLAY_HEIGHT - DISPLAY_HEADER);
     }
     else {
-      delta_per_pixel = 1;
+      delta_per_pixel = 1.0;
     }
     isDrawn = false;
   }
@@ -144,19 +138,19 @@ class TemperatureArray {
     else{
       t = (values[position] - minimum) / delta_per_pixel;
     }
-    return display_height - round(t);
+    return DISPLAY_HEIGHT - ceil(t);
   }
 
 };
 
-TemperatureArray temperatureArray = TemperatureArray(64, 16);
+TemperatureArray temperatureArray = TemperatureArray();
 
 void readTemperature(void){
   sensors.requestTemperatures(); 
   temperatureArray.add(sensors.getTempCByIndex(0));
 
   _radioData.OnTimeMillis = (int)(sensors.getTempCByIndex(0)*1000);
-  digitalWrite(LEDPIN, !_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData)));
+  digitalWrite(PIN_LED, !_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData)));
 }
 
 Task ttemp = Task(*readTemperature, 1000, "temp");
@@ -168,7 +162,7 @@ unsigned int long_press_threshold = 1500;
 unsigned long button_timer = 0;
 
 void readButton() {
-  int new_state = digitalRead(BUTTONPIN);
+  int new_state = digitalRead(PIN_BUTTON);
   if (new_state != old_state) {
     if (new_state == LOW) {
       button_timer = millis();
@@ -188,16 +182,14 @@ void readButton() {
 
 Task tbutton = Task(*readButton, 50, "button");
 
-bool ledState = false;
-
 void setup() {
   Serial.begin(115200);
   
-  if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN)){
+  if (!_radio.init(SOURCE_RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN)){
     Serial.println("Cannot communicate with radio");
-    while (1); // Wait here forever.
+    while (1);
   }  
-  _radioData.FromRadioId = RADIO_ID;
+  _radioData.FromRadioId = SOURCE_RADIO_ID;
   
   sensors.begin();
   sensors.setResolution(TEMPRESOLUTION);
@@ -206,8 +198,8 @@ void setup() {
   u8g2.setFont(u8g2_font_5x8_tf);
   u8g2.setColorIndex(1);
   
-  pinMode(BUTTONPIN, INPUT_PULLUP);
-  pinMode(LEDPIN, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  pinMode(PIN_LED, OUTPUT);
   readTemperature();
 }
 
@@ -233,7 +225,7 @@ void loop() {
       u8g2.setCursor(0, 14);
       u8g2.print("L " + String(temperatureArray.minimum, FLOAT_LENGTH)); 
       u8g2.setCursor(100, 6);
-      u8g2.print(String(temperatureArray.values[(temperatureArray.current + 127)%128], FLOAT_LENGTH)); 
+      u8g2.print(String(temperatureArray.values[(temperatureArray.current + 127)%DISPLAY_WIDTH], FLOAT_LENGTH)); 
       if (ttemp.interval < 60000){
         duration = ttemp.interval / 1000.0;
         text = "s"; 
@@ -247,9 +239,9 @@ void loop() {
         text = "h"; 
       }
       u8g2.setCursor(54, 14);
-      u8g2.print(String(duration) + text + " " + String((int)(duration*128)) + text);
-      for (int i=0; i<128;i++){
-        unsigned int val = temperatureArray.get_val((temperatureArray.current - i + 127)%128);
+      u8g2.print(String(duration) + text + " " + String((int)(duration*DISPLAY_WIDTH)) + text);
+      for (int i=0; i<DISPLAY_WIDTH;i++){
+        unsigned int val = temperatureArray.get_val((temperatureArray.current - i + 127)%DISPLAY_WIDTH);
         u8g2.drawPixel(127 - i, val);
       }
     } while ( u8g2.nextPage() );
